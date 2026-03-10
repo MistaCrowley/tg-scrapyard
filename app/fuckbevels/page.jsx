fc524710203dbbdc630a6b4f8d107f9276d1e86b
MistaCrowley <g14n7.5qu1d@gmail.com>
2026-03-10 01:16:10 -0400

    added a friendly message about bevels

---
+++ app/fuckbevels/page.jsx
@@ -0,0 +1,116 @@
+"use client"
+
+const Page = () => {
+
+	return (
+		<div className="
+			flex 
+			items-center justify-top text-left
+			grid grid-rows-20 grid-cols-20
+			gap-3
+			w-screen
+			h-screen
+			pt-10
+		"
+
+		>
+			<div className=" 
+			bg-red-600
+			col-span-4
+			col-start-3
+			row-start-1
+			"> 
+			F U C K  &nbsp;&nbsp;  B E V E L S 
+			</div>
+
+			<div className="
+				bg-[rgba(100,80,200,0.4)]
+				text-[rgba(255,255,255,0.7)]
+				italic
+				p-2
+				col-span-10
+				col-start-2
+				row-start-8
+			">
+				bevels everywhere <br/>
+				bevels on everything <br/>
+				bevels on text boxes <br/>
+				bevels on forms <br/>
+				bevels on buttons borders boundaries blockades <br/>
+				bevels on the borders <br/>
+				bevels on bullets belts bevels on knives bevels on newgrounds.com <br/>
+				bevels on belayers belaying on climbers climbing beveled rocks and never reaching the tip of the top of the rock <br/>
+				bevels on your mom <br/>
+				bevels on your dog bevels on your acceptance speech bevels on your job offer <br/>
+				bevels on your house<br/>
+				no corners, every wall is every wall <br/>
+				bevels walling you in, encasing you in a beveled cube <br/>
+				bevels on your bosses tongue when he lathers you up with all the roundeabout reasons why<br/>
+				he can't pay your ass anymore<br/>
+				bevels on the padlock when your house is foreclosed <br/>
+				bevels on the billie club that beats you back into place <br/>
+				bevels on Colin's story, the one who sold you out <br/>
+				bevels on the jailhouse cocks <br/>
+				bevels on all your edges now <br/>
+				are you a nice productive, well rounded member of society yet? <br/>
+				bevels on your hemorroid pillow <br/>
+				bevels on the gaping ulcers in your colon <br/>
+				bevels on the pill we got for that <br/>
+				bevels on your brain now <br/>
+				so many bevels on your brain <br/>
+			</div>
+			<div className="
+				bg-[rgba(255,255,255,0.1)]
+				text-[rgba(255,0,0,.8)]
+				col-span-14
+				col-start-5
+				row-start-10
+				leading-5
+				text-[20px]
+			">
+				BEVELS IN YOUR HOUSE <br/>
+				BEVELS ON ALL YOUR FURNITURE <br/>
+				BEVELS ARE THE EQUIVALENT OF ADULTS <br/>
+				FULL GROWN ASS ADULTS <br/>
+				ADDING POOL NOODLE CORNERS TO ALL THEIR FURNITURE <br/>
+				CAUSE THEIR FURNITURE IS JUST <br/>
+				TOoo<br/>
+				 S P I C Y <br/>
+				BEVELS IS LIKE MAKIN MAC N CHEESE WITH SKIM MILK <br/>
+				BEVELS IS LIKE BEING TOO AFRAID TO TURN YOUR WHIP THAT YOU CAN ONLY MAKE TURNS AT ROUNDABOUTS <br/>
+				BEVELS IS LIKE VOTING FOR HILLARY CLINTON <br/>
+				just give her a chance already <br/>
+				BEVELS IS LIKE JUST LISTENING TO ALL THE SHITTY MUSIC YOUR COWORKERS LIKE<br/>
+				but if it's them, well, it sucks, but if everyone likes it, they can't all be wrong? right?<br/>
+				riiiight?........<br/>
+				BEVELS IS LIKE YOU GO TO SCHOOL FOR 22 YEARS, A SOLID B STUDENT, <br/>
+				YOU GRADUATE <br/>
+				YOU GET A JOB YOU DON'T ENTIRELY HATE <br/>
+				YOU WORK 9-5<br/>
+				but of course you work a little unpaid over time, <br/>
+				just to complete the project<br/>
+				YOU SAVE UP A BIT OF CASH. YOU MARRY SOMEONE WHO IS KINDA OK<br/>
+				YOU FIGHT OVER WHETHER KOHLS OR WALMART IS BETTER<br/>
+				YOU HAVE 2 KIDS <br/>
+				YOU RETIRE TO FLORIDA<br/>
+				YOU DIE<br/>
+				THEY BURY YOU IN A BEVELED COFFIN AND YOU SLIDE OFF THE SHORE IN THE NEXT HURRICANE AND SLOWLY SLIDE OFF TO SEA<br/>
+				TO POLLUTE THE OCEAN <br/> 
+				WITH YOUR EMBALMED, BEVELED ASS COPRSE <br/>
+				DONT BE A CORNER COWARD<br/>
+				EMBRACE HARD LINES THAT COME TO A POINT <br/>
+				MAKE YOUR POINT. TAKE A STAND <br/>
+				DONT SLIP OFF INTO THE ROUNDED EDGE OF NO BEGINNING AND NO END<br/>
+				PIVOT. NINETY FUCKING DEGREES. CHANGE YOUR FUCKING LIFE <br/>
+				LOVE YOUR BEAUTIFUL WIFE WITH ALL OF YOUR SOUL<br/>
+				DONT BE A CORNER COWARD<br/>
+				SAY IT WITH ME NOW<br/>
+				FFFFFFFFUCK BEVELS!!! FFFFFFFFUCK BEVELS!!! FFFFFFFFUCK BEVELS!!! FFFFFFFFUCK BEVELS!!!<br/>
+			</div>
+		</div>
+	)
+
+}
+
+
+export default Page
